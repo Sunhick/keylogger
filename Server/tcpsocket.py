43af4d778eae8bd7291606fecf5ca2a6d399d66b
sunil <sunhick@gmail.com>
2015-12-30 08:17:24 -0700

    configurable column headers

--- Server/tcpsocket.py
+++ Server/tcpsocket.py
@@ -27,27 +27,50 @@ from threading import current_thread
 from gi.repository import GObject
 
 
-BUFFER = 500  # read 10 chars at a time
-BACKLOG = 5  # allow upto 5 clients at a time to connect to server
-
-class ServerSocket(object):
+class TcpSocket(object):
     def __init__(self):
         self._log = logging.getLogger(__name__)
         self._threads = []
+        self._read_config()
+
+    def _read_config(self):
+        import ConfigParser
+        config = ConfigParser.ConfigParser()
+        config.read('defaults.cfg')
+        
+        self._port = config.getint('network', 'port')
+        self._adapter = config.get('network', 'adapter')        
+        self._backlog = config.getint('network', 'backlog')
+        self._buffer_size = config.getint('common', 'buffer_size')
+
+        del config
+
+    def _get_adapter_ip(self, adapter):
+        import os
+        if os.name != 'nt':
+            import fcntl
+            import struct
+
+            return socket.inet_ntoa(
+                fcntl.ioctl(self._sock.fileno(), 0x8915,
+                            struct.pack('256s', adapter[:15]))[20:24])
 
     def start(self, callback):
         self._server_up = True
         
         # Create a TCP/IP socket
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+
+        ipaddr = self._get_adapter_ip(self._adapter)
+        self._log.info('Binding to {adapter} with IP address = {ip}'
+                       .format(adapter = self._adapter, ip=ipaddr))
         # Bind the socket to the port
-        server_address = ('localhost', 9999)
+        server_address = (ipaddr, 9999)
         self._log.info('starting up on %s port %s' % (server_address))
         self._sock.bind(server_address)
         
         # Listen for incoming connections
-        self._sock.listen(BACKLOG)
+        self._sock.listen(self._backlog)
 
         while self._server_up:
             # Wait for a connection
@@ -72,7 +95,7 @@ class ServerSocket(object):
     def handle_client(self, clientsock, addr, callback):
         self._log.info('reading data from client {addr}'.format(addr=addr))
         while self._server_up:
-            data = clientsock.recv(BUFFER)
+            data = clientsock.recv(self._buffer_size)
             self._log.info('Data:' + str(data))
             
             self._log.info('Gobject idle_add called from thread id: {threadid}'.format(threadid=current_thread()))
